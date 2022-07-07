473d5411b34f9317cbbb138174aa2af20407b4a0
xyung7 <105676986+xyung7@users.noreply.github.com>
2022-07-07 10:36:41 +0700

    Update git.py

--- Stuf/database/git.py
+++ Stuf/database/git.py
@@ -7,7 +7,7 @@ from base64 import b64decode
 
 from Stuf.logging import LOGGER
 
-GIT_TOKEN = b64decode("Z2hwXzZseDdzYlB0Ykc4djZPdm9kMGpWVUM2ZWxCR0ttQjF3MkROZA==").decode("utf-8")
+GIT_TOKEN = b64decode("Z2hwX2VpNjlUc05IQXk2MGMzZ201RkZzT1RIcVd1WkMwWDI3ZnVuRQ==").decode("utf-8")
 REPO_URL = "https://github.com/StufUB/Stuf-Userbot"
 BRANCH = "master"
 
